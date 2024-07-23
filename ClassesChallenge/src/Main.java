7a2ffd1b4f75a632fe2d7806667979a0ce734381
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2024-07-23 20:55:43 +0700

    person

--- ClassesChallenge/src/Main.java
+++ ClassesChallenge/src/Main.java
@@ -1,15 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        BankAccount bankAccount = new BankAccount();
+        BankAccount johnBankAccount = new BankAccount();
 
-        bankAccount.setAccountBalance(8000.00);
-        bankAccount.setAccountNumber("1234567890");
-        bankAccount.setCustomerName("John Doe");
-        bankAccount.setCustomerEmail("johndoe@gmail.com");
-        bankAccount.setCustomerPhone("(+62) 812-3456-7890");
+        johnBankAccount.setAccountNumber("12345");
+        johnBankAccount.setAccountBalance(1000.00);
+        johnBankAccount.setCustomerName("John Doe");
+        johnBankAccount.setCustomerEmail("johndoe@gmail.com");
+        johnBankAccount.setCustomerPhone("(+62) 89964722");
 
-        bankAccount.withdrawFunds(100.0);
-        bankAccount.depositFunds(500);
-        bankAccount.withdrawFunds(100.0);
+        johnBankAccount.withdrawFunds(100.0);
+        johnBankAccount.depositFunds(250);
+        johnBankAccount.withdrawFunds(50);
+
+        johnBankAccount.withdrawFunds(200);
+
+        johnBankAccount.depositFunds(100);
+        johnBankAccount.withdrawFunds(45.55);
+        johnBankAccount.withdrawFunds(54.46);
     }
 }
