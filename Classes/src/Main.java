02aa6a83f459a90e926d680d6d4ace42336f8611
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2024-07-23 09:45:22 +0700

    setter

--- Classes/src/Main.java
+++ Classes/src/Main.java
@@ -1,9 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         Car car = new Car();
-        car.describeCar();
+
+        car.setMake("Toyota");
+        car.setModel("Avanza");
+        car.setColor("Silver");
+        car.setDoors(2);
+        car.setConvertible(true);
 
         System.out.println("make = " + car.getMake());
         System.out.println("model = " + car.getModel());
+        car.describeCar();
     }
 }
