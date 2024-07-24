257ea22d7d23189e51a726e8b5bd8b1d35c3fbbe
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2024-07-24 20:17:30 +0700

    POJO Java and the record

--- RecordProject/src/Main.java
+++ RecordProject/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         for (int i = 1; i <= 5; i++) {
-            Student student = new Student("S92300" + i,
+            LPAStudent student = new LPAStudent("S92300" + i,
                     switch (i) {
                         case 1 -> "Mary";
                         case 2 -> "Jane";
@@ -16,5 +16,13 @@ public class Main {
             System.out.println(student);
         }
 
+        Student pojoStudent = new Student("S923006", "Ann", "05/11/1985", "Java Masterclass");
+        LPAStudent lpaStudent = new LPAStudent("S923007", "Bill", "05/11/1985", "Java Masterclass");
+
+        pojoStudent.setClassList(pojoStudent.getClassList() + ", Java OCP Exam 829");
+//        lpaStudent.setClassList(lpaStudent.classList() + ", Java OCP Exam 829");
+
+        System.out.println(pojoStudent.getName() + " is taking " + pojoStudent.getClassList());
+        System.out.println(lpaStudent.name() + " is taking " + lpaStudent.classList());
     }
 }
