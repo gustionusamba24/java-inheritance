3d5ccca5acdda2d8e082b2f9f48e6411c8fc7d4c
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2024-07-24 20:49:01 +0700

    finish wall area challenge

--- JavaInheritanceExercise/src/Main.java
+++ JavaInheritanceExercise/src/Main.java
@@ -10,17 +10,23 @@ public class Main {
 //        System.out.println("multiply= " + calculator.getMultiplicationResult());
 //        System.out.println("divide= " + calculator.getDivisionResult());
 
-        Person person = new Person();
-        person.setFirstName("");
-        person.setLastName("");
-        person.setAge(10);
-        System.out.println("fullName= " + person.getFullName());
-        System.out.println("teen= " + person.isTeen());
-        person.setAge(18);
-        System.out.println("fullName= " + person.getFullName());
-        System.out.println("teen= " + person.isTeen());
-        person.setLastName("Smith");
-        System.out.println("fullName= " + person.getFullName());
-    }
+//        Person person = new Person();
+//        person.setFirstName("");
+//        person.setLastName("");
+//        person.setAge(10);
+//        System.out.println("fullName= " + person.getFullName());
+//        System.out.println("teen= " + person.isTeen());
+//        person.setAge(18);
+//        System.out.println("fullName= " + person.getFullName());
+//        System.out.println("teen= " + person.isTeen());
+//        person.setLastName("Smith");
+//        System.out.println("fullName= " + person.getFullName());
 
+        Wall wall = new Wall(5, 4);
+        System.out.println("area= " + wall.getArea());
+        wall.setHeight(-1.5);
+        System.out.println("width= " + wall.getWidth());
+        System.out.println("height= " + wall.getHeight());
+        System.out.println("area= " + wall.getArea());
+    }
 }
