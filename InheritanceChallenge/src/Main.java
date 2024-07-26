9f2c1c8618e77494d38ebe53cf6f12c006d39752
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2024-07-26 09:51:18 +0700

    inheritance challenge pt. 2

--- InheritanceChallenge/src/Main.java
+++ InheritanceChallenge/src/Main.java
@@ -5,7 +5,16 @@ public class Main {
         System.out.println("Collected Pay " + jane.collectPay());
         System.out.println(jane);
 
-        Employee john = new Employee("John", "06/05/1984", "02/02/2019");
+        SalariedEmployee john = new SalariedEmployee("John", "06/05/1984", "02/02/2019", 35000);
         System.out.println(john);
+        System.out.println("John's Paycheck = $" + john.collectPay());
+
+        john.retire();
+        System.out.println("John's pension check = $" + john.collectPay());
+
+        HourlyEmployee mary = new HourlyEmployee("Mary", "07/07/1978", "04/04/2023", 15);
+        System.out.println(mary);
+        System.out.println("Mary's PayCheck = $" + mary.collectPay());
+        System.out.println("Mary's Holiday Pay = $" + mary.getDoublePay());
     }
 }
